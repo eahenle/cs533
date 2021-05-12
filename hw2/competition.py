31fea8ac4a75b430a397b3e05a2e0711cf77f5d9
Adrian Henle <eahenle@gmail.com>
2021-05-12 12:19:11 -0700

    additional maps

--- hw2/competition.py
+++ hw2/competition.py
@@ -223,13 +223,161 @@ def fast_value_iteration(env, beta = 0.999, epsilon = 0.01, workers_num = 4):
     return ray.get(VI_server.get_value_and_policy.remote())
 
 
-if __name__ == "__main__":
-    MAPS = {"4x4": [
+def generate_row(length, h_prob):
+    row = np.random.choice(2, length, p=[1.0 - h_prob, h_prob])
+    row = ''.join(list(map(lambda z: 'F' if z == 0 else 'H', row)))
+    return row
+
+
+def generate_map(shape):
+    """
+
+    :param shape: Width x Height
+    :return: List of text based map
+    """
+    h_prob = 0.1
+    grid_map = []
+
+    for h in range(shape[1]):
+
+        if h == 0:
+            row = 'SF'
+            row += generate_row(shape[0] - 2, h_prob)
+        elif h == 1:
+            row = 'FF'
+            row += generate_row(shape[0] - 2, h_prob)
+
+        elif h == shape[1] - 1:
+            row = generate_row(shape[0] - 2, h_prob)
+            row += 'FG'
+        elif h == shape[1] - 2:
+            row = generate_row(shape[0] - 2, h_prob)
+            row += 'FF'
+        else:
+            row = generate_row(shape[0], h_prob)
+
+        grid_map.append(row)
+        del row
+
+    return grid_map
+
+
+
+MAPS = {
+    
+    "4x4": [
         "SFFF",
         "FHFH",
         "FFFH",
         "HFFG"
-    ]}
+    ],
+    "8x8": [
+        "SFFFFFFF",
+        "FFFFFFFF",
+        "FFFHFFFF",
+        "FFFFFHFF",
+        "FFFHFFFF",
+        "FHHFFFHF",
+        "FHFFHFHF",
+        "FFFHFFFG"
+    ],
+    "16x16": [
+        "SFFFFFFFFHFFFFHF",
+        "FFFFFFFFFFFFFHFF",
+        "FFFHFFFFHFFFFFFF",
+        "FFFFFFFFHFFFFFFF",
+        "FFFFFFFFFFFFFFFF",
+        "FFHHFFFFFFFHFFFH",
+        "FFFFFFFFFFFFFFFF",
+        "FFFFFHFFFFFFHFFF",
+        "FFFFFHFFFFFFFFFH",
+        "FFFFFFFHFFFFFFFF",
+        "FFFFFFFFFFFFHFFF",
+        "FFFFFFHFFFFFFFFF",
+        "FFFFFFFFHFFFFFFF",
+        "FFFFFFFFFHFFFFHF",
+        "FFFFFFFFFFHFFFFF",
+        "FFFHFFFFFFFFFFFG",
+    ],
+    
+    "32x32": [
+        'SFFHFFFFFFFFFFFFFFFFFFFFFFHFFFFF',
+        'FFHFHHFFHFFFFFFFFFFFFFFFFFHFFFFF',
+        'FFFHFFFFFFFFHFFHFFFFFFFFFFFFFFFF',
+        'FFFFFFFFFFFFFFHFHHFHFHFFFFFHFFFH',
+        'FFFFHFFFFFFFFFFFFFFFHFHFFFFFFFHF',
+        'FFFFFHFFFFFFFFFFHFFFFFFFFFFHFFFF',
+        'FFHHFFFFHFFFFFFFFFFFFFFFFFFFFFFF',
+        'FFFHFFFFFFFFFFHFFFHFHFFFFFFFFHFF',
+        'FFFFHFFFFFFHFFFFHFHFFFFFFFFFFFFH',
+        'FFFFHHFHFFFFHFFFFFFFFFFFFFFFFFFF',
+        'FHFFFFFFFFFFHFFFFFFFFFFFHHFFFHFH',
+        'FFFHFFFHFFFFFFFFFFFFFFFFFFFFHFFF',
+        'FFFHFHFFFFFFFFHFFFFFFFFFFFFHFFHF',
+        'FFFFFFFFFFFFFFFFHFFFFFFFHFFFFFFF',
+        'FFFFFFHFFFFFFFFHHFFFFFFFHFFFFFFF',
+        'FFHFFFFFFFFFHFFFFFFFFFFHFFFFFFFF',
+        'FFFHFFFFFFFFFHFFFFHFFFFFFHFFFFFF',
+        'FFFFFFFFFFFFFFFFFFFFFFFFFFHFFFFF',
+        'FFFFFFFFHFFFFFFFHFFFFFFFFFFFFFFH',
+        'FFHFFFFFFFFFFFFFFFHFFFFFFFFFFFFF',
+        'FFFFFFFHFFFFFFFFFFFFFFFFFFFFFFFF',
+        'FFFFFFFFFFFFFFFHFFFFHFFFFFFFHFFF',
+        'FFHFFFFHFFFFFFFFFHFFFFFFFFFFFHFH',
+        'FFFFFFFFFFHFFFFHFFFFFFFFFFFFFFFF',
+        'FFFFFFFFFFFFFFFFFHHFFHHHFFFHFFFF',
+        'FFFFFFFFFFFFFFHFFFFHFFFFFFFHFFFF',
+        'FFFFFFFHFFFFFFFFFFFFFFFFFFFFFFFF',
+        'FFFFFHFFFFFFFFFFFFFFFFHFFHFFFFFF',
+        'FFFFFFFHFFFFFFFFFHFFFFFFFFFFFFFF',
+        'FFFFFFFFFFFFFFFFFFFFFFFFHFFFFFFF',
+        'FFFFFFFFFFFFFFFFFFFFFFFFHFFFFFFF',
+        'FFFFFFFFFFFFFFFHFFFFFFFFHFFFFFFG',
+    ]
+}
+
+
+def generate_random_map(size=8, p=0.8):
+    """Generates a random valid map (one that has a path from start to goal)
+    :param size: size of each side of the grid
+    :param p: probability that a tile is frozen
+    """
+    valid = False
+
+    # BFS to check that it's a valid path.
+    def is_valid(arr, r=0, c=0):
+        if arr[r][c] == 'G':
+            return True
+
+        tmp = arr[r][c]
+        arr[r][c] = "#"
+
+        # Recursively check in all four directions.
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        for x, y in directions:
+            r_new = r + x
+            c_new = c + y
+            if r_new < 0 or r_new >= size or c_new < 0 or c_new >= size:
+                continue
+
+            if arr[r_new][c_new] not in '#H':
+                if is_valid(arr, r_new, c_new):
+                    arr[r][c] = tmp
+                    return True
+
+        arr[r][c] = tmp
+        return False
+
+    while not valid:
+        p = min(1, p)
+        res = np.random.choice(['F', 'H'], (size, size), p=[p, 1-p])
+        res[0][0] = 'S'
+        res[-1][-1] = 'G'
+        valid = is_valid(res)
+    return ["".join(x) for x in res]
+
+
+if __name__ == "__main__":
     map_4 = (MAPS["4x4"], 4)
     MAP = map_4
     map_size = MAP[1]
