0ec5ae88c777d6d2ccccd37ef8eb0934e8b9faf0
Adrian Henle <eahenle@gmail.com>
2021-06-07 16:20:03 -0700

    timing & performance

---
+++ hw4_v6/run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# make sure code is up-to-date
+git pull
+
+# run tests on {1, 2, 4, 8} collectors w/ 2 evaluators, {1, 2, 4, 8} evaluators w/ 4 collectors
+
+for nb_collectors in 1, 2, 4, 8
+do
+    ./distributed_dqn.py $nb_collectors 2 > $nb_collectors-2.txt &
+done
+
+for nb_evaluators in 1, 4, 8
+do
+    ./distributed_dqn.py 4 $nb_evaluators > 4-$nb_evaluators.txt &
+done
+
+watch ps
